be5987e57b610b932254a9ab145f628ddf630f7e
Adam Vogt <vogt.adam@gmail.com>
2013-10-06 19:19:09 +0100

    add hTranspose
    Ignore-this: 30e7f07a91d4933daf101de4caac8d4d
    
    darcs-hash:5c1ba7b198ebdd1f2f690b9c74303ca305655d35

--- Data/HList/HZip.hs
+++ Data/HList/HZip.hs
@@ -12,8 +12,10 @@
 module Data.HList.HZip where
 
 import Data.HList.HList
+import Data.HList.FakePrelude
 
--- * functional dependency
+-- * zip
+-- ** functional dependency
 
 class HZip x y l | x y -> l, l -> x y where
   hZip   :: HList x -> HList y -> HList l
@@ -27,7 +29,7 @@ instance ((x,y)~z, HZip xs ys zs) => HZip (x ': xs) (y ': ys) (z ': zs) where
   hZip (HCons x xs) (HCons y ys) = (x,y) `HCons` hZip xs ys
   hUnzip (HCons ~(x,y) zs) = let ~(xs,ys) = hUnzip zs in (x `HCons` xs, y `HCons` ys)
 
--- * type family
+-- ** type family
 -- $note 'hZip2' can be written as a standalone function, with an appropriate
 -- type family to calculate the result type. However, that does not seem to
 -- be the case for 'hUnzip2', so to re-use some type functions the two are
@@ -68,10 +70,46 @@ type instance HZipR (x ': xs) (y ': ys) = (x,y) ': HZipR xs ys
 
 
 -- ** utility type functions
--- $note these could be polykinded. Also do they belong somewhere else?
+-- $note do they belong somewhere else?
 type family Fst a
 type instance Fst (a,b) = a
 type family Snd a
 type instance Snd (a,b) = b
 
 
+-- * transpose
+
+{- | this transpose requires equal-length HLists inside a HList:
+
+>>> let ex = (1 .*. 2 .*. HNil) .*. ('a' .*. 'b' .*. HNil) .*. ( 3 .*. 5 .*. HNil) .*. HNil
+>>> ex
+H[H["b", "a"], H['a', 'b'], H[(), 5]]
+
+
+>>> hTranspose samp
+H[H["b", 'a', ()], H["a", 'b', 5]]
+
+-}
+hTranspose x = hFoldr HZipF (hReplicate (hLength (hHead x)) HNil) x
+
+
+-- ** helpers
+
+-- | same as 'HZip' but HCons the elements of x onto y. This might be doable as
+-- a @hMap f (hZip x y)@, but that one doesn't propagate types as easily it
+-- seems.
+class HZip3 x y l | x y -> l, l -> x y where
+  hZip3   :: HList x -> HList y -> HList l
+
+instance HZip3 '[] '[] '[] where
+  hZip3 _ _ = HNil
+
+instance (HList (x ': y) ~z, HZip3 xs ys zs) => HZip3 (x ': xs) (HList y ': ys) (z ': zs) where
+  hZip3 (HCons x xs) (HCons y ys) = HCons x y  `HCons` hZip3 xs ys
+
+data HZipF = HZipF
+instance (
+    HZip3 a b c,
+    x ~ (HList a, HList b),
+    y ~ HList c) => ApplyAB HZipF x y
+    where applyAB _ (x,y) = hZip3 x y
