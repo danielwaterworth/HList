83a3a9ed747c84c46113c702c288f131803b659c
Adam Vogt <vogt.adam@gmail.com>
2013-10-06 16:37:15 +0100

    document runner for doctests (and allow overriding the default files to check)
    Ignore-this: 6ea2971db4d4c204a954f471a165523a
    
    darcs-hash:601698ee2488cefda086360914c6f20dc97cc533

--- examples/rundoctests.hs
+++ examples/rundoctests.hs
@@ -3,15 +3,35 @@ import System.Exit
 import System.IO
 import Test.DocTest
 import Data.Char
+import System.Environment
 
+-- | tests that all the >>> comments are followed by correct output. Easiest is to
+--
+-- > cabal test
+--
+-- or
+--
+-- > runghc examples/rundoctests.hs
+--
+-- or
+--
+-- > runghc examples/rundoctests.hs Data/HList/File1.hs Data/HList/File2.hs
+--
+-- you need Cabal >= 1.18 since that's around when cabal repl got added.
 main = do
+    as <- getArgs
     o <- readProcess
-        "cabal" ["repl","--ghc-options","-v0"]
+        "cabal" ["repl","--ghc-options","-v0 -w"]
         ":show packages\n:show language"
     let flags = words $ unlines $ filter ((=="-") . take 1 . dropWhile isSpace)
                     $ lines o
 
-    doctest $ "-i.": "-idist/build/autogen": 
+    let files = case as of
+            [] -> ["Data/HList/CommonMain.hs"]
+            _ -> as
+
+    doctest $ "-i.": "-idist/build/autogen":
              "-optP-include":
              "-optPdist/build/autogen/cabal_macros.h" :
-             "-Idist/build/autogen" : "-w": "Data/HList/CommonMain.hs": flags
+             "-Idist/build/autogen" : "-w":
+             files ++ flags
