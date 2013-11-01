1b673f6cf5301a3f39ca45a2b5a630c163869301
Adam Vogt <vogt.adam@gmail.com>
2013-11-01 19:59:34 +0000

    support nesting in pun quasiquote
    Ignore-this: 1fccbd1281509c58e2338709daa9f156
    
    darcs-hash:d91ab03a87681653935fd9c11b80264138f7e582

--- Data/HList/RecordPuns.hs
+++ Data/HList/RecordPuns.hs
@@ -1,37 +1,17 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoPolyKinds #-}
-{- | Description : quasiquoter emulating -XNamedFieldPuns -}
+{- | Description : quasiquoter inspired by -XNamedFieldPuns -}
 module Data.HList.RecordPuns (
     -- $ex
     pun
 
-    -- * todo
-    {- |
-    nesting is not supported. In principle it might be possible to
-    support the following
-
-    > [rec| ab@{ a b } y z c{d} |]
-
-    which might be a pattern match which makes bindings from an
-    original record @x@, according to the following look-ups (using @.@ for
-    lookup)
-
-    > let ab = x.ab
-    >     a = x.ab.a
-    >     b = x.ab.b
-    >     y = x.y
-    >     d = x.c.d
-
-    Or it could be an expression which creates a record,
-    using the values @ab a b y d@ which are in scope.
-
-    -}
     ) where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Data.HList.Record
 import Data.HList.FakePrelude
+import Data.List
 
 
 {- $ex
@@ -54,27 +34,46 @@ Compare with the standard way to construct records above
 >>> [pun|x y|]
 Record{x=3,y="hi"}
 
+[@nesting@]
+
+Nesting is supported. The idea is that variables inside
+@{ }@ are in another record. More concretely:
+
+> [pun| ab@{ a b } y z c{d} |]
+
+as a pattern, it will bindings from an original record @x@,
+if you interpret (.) as a left-associative field lookup (as it
+is in other languages):
+
+> let ab = xab
+>     a = x.ab.a
+>     b = x.ab.b
+>     y = x.y
+>     z = x.z
+>     -- c is not bound
+>     d = x.c.d
+
+as an expression, it creates a new record which needs the variables
+@ab a b y z d@ in-scope. @ab@ needs to be a record, and if it has
+fields called @a@ or @b@ they are overridden by the values of @a@ and @b@
+which are in scope.
+
+See also @examples/pun.hs@.
 -}
 
 
 -- | requires the use of "Data.HList.Label6" (ie. the label for foo is @Label :: Label \"foo\"@)
 pun :: QuasiQuoter
 pun = QuasiQuoter {
-    quotePat = \str -> mkPat (words str),
-    quoteExp  = \str -> mkExp (words str),
+    quotePat = mp . parseRec,
+    quoteExp = me . parseRec,
     quoteDec  = error "Data.HList.RecordPuns.quoteDec",
     quoteType = error "Data.HList.RecordPuns.quoteType"
  }
 
-mkPat :: [String] -> PatQ
-mkPat xs = viewP extracts binds
-  where
 
-  -- like (x1,x2,x3)
-  binds = tupP (map (varP . mkName) xs)
-
-  -- like  \x -> (x .!. x1, x .!. x2)
-  extracts = do
+-- like  \x -> (x .!. x1, x .!. x2)
+extracts xs = do
     record <- newName "record"
     lamE [varP record]
         (tupE
@@ -84,7 +83,76 @@ mkPat xs = viewP extracts binds
                 ])
 
 mkExp :: [String] -> ExpQ
-mkExp xs = foldr (\x acc -> [| $(mkPair x) .*. $acc |]) [| emptyRecord |] xs
-  where
-  mkPair :: String -> ExpQ
-  mkPair x = [| (Label :: Label $(litT (strTyLit x))) .=. $(dyn x) |]
+mkExp xs = foldr (\x acc -> [| $(mkPair x (dyn x)) .*. $acc |]) [| emptyRecord |] xs
+
+mkPair :: String -> ExpQ -> ExpQ
+mkPair x xe = [| (Label :: Label $(litT (strTyLit x))) .=. $xe |]
+
+
+
+me :: Tree -> ExpQ
+me (C as) = foldr (\(l,e) acc -> [| $(mkPair l e) .*. $acc |]) [| emptyRecord |] (mes as)
+me a = do
+    reportWarning $ "Data.HList.RecordPuns.mp implicit {} added around:" ++ show a
+    me (C [a])
+
+mes :: [Tree] -> [(String, ExpQ)]
+mes (V a : V "@": b : c) = (a, [| $(me b) `hLeftUnion` $(dyn a) |]) : mes c
+mes (V a : C b : c)      = (a, me (C b)) : mes c
+mes (V a : b)            = (a, varE (mkName a)) : mes b
+mes [] = []
+mes inp = error $ "Data.HList.RecordPuns.mes: cannot translate remaining:" ++
+                        show (map ppTree inp)
+
+mp :: Tree -> PatQ
+mp (C as) = case unzip (mps as) of
+    (a, b) -> viewP (extracts a) (tupP b)
+mp a = do
+    reportWarning $ "Data.HList.RecordPuns.mp implicit {} added around:" ++ show a
+    mp (C [a])
+
+mps :: [Tree] -> [(String, PatQ)]
+mps (V a : V "@" : b : c) = (a, asP (mkName a) (mp b)) :  mps c
+mps (V a : C b : c) = (a, mp (C b)) : mps c
+mps (V a : b) = (a, varP (mkName a)) : mps b
+mps [] = []
+mps inp = error $ "Data.HList.RecordPuns.mps: cannot translate remaining pattern:" ++
+                        show (map ppTree inp)
+
+
+data Tree = C [Tree] | V String deriving Show
+
+{- |
+
+>>> parseRec "{ a b c {d e f}  } d"
+C [C [V "a",V "b",V "c",C [V "d",V "e",V "f"]],V "d"]
+
+>>> ppTree $ parseRec "{a b c {d e {} f @ g}}"
+"{a b c {d e {} f @ g}}"
+
+> ppTree $ parseRec "a b c {d e {} f @ g}"
+"{a b c {d e {} f @ g}}"
+
+-}
+parseRec :: String -> Tree
+parseRec str = case parseRec' 0 [[]] $ lexing str of
+    [x] -> x -- avoid adding another layer if possible
+    x -> C (reverse x)
+
+parseRec' :: Int -> [[Tree]] -> [String] -> [Tree]
+parseRec' n accum  ("{" : rest)  = parseRec' (n+1) ([] : accum) rest
+parseRec' n (a:b:c) ("}" : rest) = parseRec' (n-1) ((C (reverse a) : b) : c)  rest
+parseRec' n (b:c) (a   : rest)
+         | a `notElem` ["{","}"] = parseRec' n     ((V a : b) : c) rest
+parseRec' 0 (a:_) []             = a
+parseRec' _ accum e              = error ("Data.HList.RecordPuns.parseRec' unexpected: " ++ show e
+                                            ++ "\n parsed:" ++ show (reverse accum))
+
+ppTree :: Tree -> String
+ppTree (C ts) = "{" ++ unwords (map ppTree ts) ++ "}"
+ppTree (V x)  = x
+
+lexing = unfoldr (\v -> case lex v of
+                    ("", "") : _ -> Nothing
+                    e : _ -> Just e
+                    _ -> Nothing)
---
+++ examples/pun.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ViewPatterns #-}
+-- more examples for record puns
+module Main where
+import Data.HList.CommonMain
+
+makeLabels6 (words "a b c")
+
+
+r  = c .=. "c" .*. b .=. (a .=. 3 .*. emptyRecord) .*. emptyRecord
+r2 = b .=. (a .=. 1 .*. emptyRecord) .*. emptyRecord
+
+
+p1 ( (.!. b) -> (b @ ((.!. a) -> a))) = (a,b)
+
+p2 [pun| b @ {a} |] = (a, b)
+
+-- same as p2, but gives a warning
+-- p3 [pun| b @ a |] = (a, b)
+
+p4 [pun| b{a} |] = a -- b is not bound
+
+-- adds `x' and `y' into a field called r
+e1 = let x = 1; y = "hi" in [pun| r @ { x y } |]
+
+-- updates the `c' field
+e2 = let c = 1; y = "hi" in [pun| r @ { c y } |]
+
+-- same as e1, but doesn't use a pre-existing r
+e3 = let x = 1; y = "hi" in [pun| r { x y } |]
+
+
+main = do
+        putStrLn "similar:"
+        print $ p1 r
+        print $ p2 r
+        print $ p4 r
+
+        putStrLn "\nexpression QQ:"
+        print $ e1
+        print $ e2
+        print $ e3
+
