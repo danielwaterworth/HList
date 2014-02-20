836eba26b85ded745e3a5bdde834ae2c85cdd185
Adam Vogt <vogt.adam@gmail.com>
2014-02-20 07:22:45 +0000

    more syntax for RecordPuns using ( )
    Ignore-this: 18e43553dda837a9d6f78fed9188441
    
    the { } syntax doesn't restrict the type "upstream", while the ( ) does.
    Hopefully this choice fits in with previous experiences: in that tuples done
    with ( ) are sensitive to order, while { } for sets/hashes/map are insensitive
    to order.
    
    darcs-hash:891756dcbe7dc647fb26b0b27b6f326b6c9d605a

--- Data/HList/RecordPuns.hs
+++ Data/HList/RecordPuns.hs
@@ -12,7 +12,7 @@ import Language.Haskell.TH.Quote
 import Data.HList.Record
 import Data.HList.FakePrelude
 import Data.List
-
+import Data.HList.HList
 
 {- $ex
 
@@ -58,6 +58,18 @@ as an expression, it creates a new record which needs the variables
 fields called @a@ or @b@ they are overridden by the values of @a@ and @b@
 which are in scope.
 
+@( )@ parens mean the same thing as @{ }@, except the pattern match
+restricts the fields in the record supplied to be exactly the ones
+provided. In other words
+
+> [pun| (x y) |] = list
+> -- desugars to something like:
+> Record (HCons (Tagged x :: Tagged "x" s1)
+>              (HCons (Tagged x :: Tagged "y" s2)
+>               HNil)) = list
+
+Where the @s1@ and @s2@ are allowed to fit whatever is in the HList.
+
 See also @examples/pun.hs@.
 -}
 
@@ -92,6 +104,7 @@ mkPair x xe = [| (Label :: Label $(litT (strTyLit x))) .=. $xe |]
 
 me :: Tree -> ExpQ
 me (C as) = foldr (\(l,e) acc -> [| $(mkPair l e) .*. $acc |]) [| emptyRecord |] (mes as)
+me (D _as) = error "Data.HList.RecordPuns.mp impossible"
 me a = do
     reportWarning $ "Data.HList.RecordPuns.mp implicit {} added around:" ++ show a
     me (C [a])
@@ -99,6 +112,7 @@ me a = do
 mes :: [Tree] -> [(String, ExpQ)]
 mes (V a : V "@": b : c) = (a, [| $(me b) `hLeftUnion` $(dyn a) |]) : mes c
 mes (V a : C b : c)      = (a, me (C b)) : mes c
+mes (V a : D b : c)      = (a, me (C b)) : mes c
 mes (V a : b)            = (a, varE (mkName a)) : mes b
 mes [] = []
 mes inp = error $ "Data.HList.RecordPuns.mes: cannot translate remaining:" ++
@@ -107,6 +121,19 @@ mes inp = error $ "Data.HList.RecordPuns.mes: cannot translate remaining:" ++
 mp :: Tree -> PatQ
 mp (C as) = case unzip (mps as) of
     (a, b) -> viewP (extracts a) (tupP b)
+
+-- use of prime here (non GADT version) because it is better for type
+-- inference. See commentary surrounding HCons' in Data.HList.HList
+mp (D as) = conP 'Record
+  [viewP (varE 'prime) -- nicer to have [p| prime -> $( ... ) |],
+                       -- but ghc-7.6 rejects that over types
+   (foldr ( \ (n,p) xs -> conP 'HCons'
+                [viewP [| \x -> x `asTypeOf`
+                            (undefined :: Tagged $(litT (strTyLit n)) t) |]
+                (conP 'Tagged [p]),
+                xs])
+          (conP 'HNil' [])
+          (mps as))]
 mp a = do
     reportWarning $ "Data.HList.RecordPuns.mp implicit {} added around:" ++ show a
     mp (C [a])
@@ -114,13 +141,16 @@ mp a = do
 mps :: [Tree] -> [(String, PatQ)]
 mps (V a : V "@" : b : c) = (a, asP (mkName a) (mp b)) :  mps c
 mps (V a : C b : c) = (a, mp (C b)) : mps c
+mps (V a : D b : c) = (a, mp (D b)) : mps c
 mps (V a : b) = (a, varP (mkName a)) : mps b
 mps [] = []
 mps inp = error $ "Data.HList.RecordPuns.mps: cannot translate remaining pattern:" ++
                         show (map ppTree inp)
 
-
-data Tree = C [Tree] | V String deriving Show
+data Tree = C [Tree] -- ^ curly @{ }@
+          | D [Tree] -- ^ @(  )@
+          | V String -- ^ variable
+  deriving Show
 
 {- |
 
@@ -130,26 +160,32 @@ C [C [V "a",V "b",V "c",C [V "d",V "e",V "f"]],V "d"]
 >>> ppTree $ parseRec "{a b c {d e {} f @ g}}"
 "{a b c {d e {} f @ g}}"
 
-> ppTree $ parseRec "a b c {d e {} f @ g}"
+>>> ppTree $ parseRec "a b c {d e {} f @ g}"
 "{a b c {d e {} f @ g}}"
+
+>>> ppTree $ parseRec "(a b { (d) e } )"
+"(a b {(d) e})"
 
 -}
 parseRec :: String -> Tree
-parseRec str = case parseRec' 0 [[]] $ lexing str of
+parseRec str = case parseRec' 0 0 [[]] $ lexing str of
     [x] -> x -- avoid adding another layer if possible
     x -> C (reverse x)
 
-parseRec' :: Int -> [[Tree]] -> [String] -> [Tree]
-parseRec' n accum  ("{" : rest)  = parseRec' (n+1) ([] : accum) rest
-parseRec' n (a:b:c) ("}" : rest) = parseRec' (n-1) ((C (reverse a) : b) : c)  rest
-parseRec' n (b:c) (a   : rest)
-         | a `notElem` ["{","}"] = parseRec' n     ((V a : b) : c) rest
-parseRec' 0 (a:_) []             = a
-parseRec' _ accum e              = error ("Data.HList.RecordPuns.parseRec' unexpected: " ++ show e
+parseRec' :: Int -> Int -> [[Tree]] -> [String] -> [Tree]
+parseRec' n m accum  ("{" : rest)  = parseRec' (n+1) m ([] : accum) rest
+parseRec' n m accum  ("(" : rest)  = parseRec'  n (m+1) ([] : accum) rest
+parseRec' n m (a:b:c) ("}" : rest) = parseRec' (n-1) m ((C (reverse a) : b) : c)  rest
+parseRec' n m (a:b:c) (")" : rest) = parseRec' n (m-1) ((D (reverse a) : b) : c)  rest
+parseRec' n m (b:c) (a   : rest)
+         | a `notElem` ["{","}","(",")"] = parseRec' n m   ((V a : b) : c) rest
+parseRec' 0 0 (a:_) []             = a
+parseRec' _ _ accum e              = error ("Data.HList.RecordPuns.parseRec' unexpected: " ++ show e
                                             ++ "\n parsed:" ++ show (reverse accum))
 
 ppTree :: Tree -> String
 ppTree (C ts) = "{" ++ unwords (map ppTree ts) ++ "}"
+ppTree (D ts) = "(" ++ unwords (map ppTree ts) ++ ")"
 ppTree (V x)  = x
 
 lexing = unfoldr (\v -> case lex v of
