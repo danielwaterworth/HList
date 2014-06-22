6cbe76ab4f4a2cfbd11efcf7bcea11c2249f0157
Adam Vogt <vogt.adam@gmail.com>
2014-06-22 04:08:11 +0100

    add LensDefs
    Ignore-this: c75024bd88c333edfc501e2b41a7c2d
    
    darcs-hash:bfcabec30e99174f59726ce4d29cd11d960a6006

---
+++ LensDefs.hs
@@ -0,0 +1,30 @@
+-- | parts of lens would be imported if we depended on it
+module LensDefs
+  (module LensDefs,
+   module Control.Applicative,
+   Choice,
+   Profunctor)
+   where
+
+import Data.Profunctor
+import Data.Profunctor.Unsafe
+import Control.Applicative
+
+simple :: p a (f a) -> p a (f a)
+simple = id
+
+iso :: (Profunctor p, Functor f)
+    => (s -> a) -> (b -> t)
+    -> p a (f b) -> p s (f t)
+iso sa bt = dimap sa (fmap bt)
+
+-- | iso, except assumes that the functions supplied could
+-- be 'Data.Coerce.coerce'
+isoNewtype :: (Profunctor p, Functor f)
+    => (s -> a) -> (b -> t)
+    -> p a (f b) -> p s (f t)
+isoNewtype sa bt x = fmap bt #. x .# sa
+
+prism :: (b -> t) -> (s -> Either t a)
+    -> (forall p f. (Choice p, Applicative f) => p a (f b) -> p s (f t))
+prism bt seta = dimap seta (either pure (fmap bt)) . right'
