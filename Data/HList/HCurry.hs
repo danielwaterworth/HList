93c6b5aa63f98d9f5b9990d4160fe216c84eb8c6
Adam Vogt <vogt.adam@gmail.com>
2015-03-21 05:05:02 +0000

    Make HCurry work with ghc-7.10 RC1
    Ignore-this: 38ec0bb8de2a4d521e169ccc3b284c7f
    
    darcs-hash:eb67b5721c7c4256711596a491398d8275cb773c

--- Data/HList/HCurry.hs
+++ Data/HList/HCurry.hs
@@ -13,19 +13,12 @@ import Data.HList.TypeEqO () -- Arity instance
 
 type HCurry f xs r = HCurry' (HLength xs) f xs r
 
--- | Ideally this would just be @HLength xs ~ n@. However,
--- the second constraint allows refining xs to '[a, b, c]
--- if n is known to be 3.
-type HLengthEq xs n =
-       (HLength xs ~ n,
-        SameLength' (HReplicateR n ()) xs)
-
 
 
 {- | 'curry'/'uncurry' for many arguments and HLists instead of tuples
 
 -}
-class (HLengthEq xs n) => HCurry' (n :: HNat) f xs r
+class HLength1 xs n => HCurry' (n :: HNat) f xs r
           | f xs -> r, r xs -> f, n f -> xs where
     hUncurry' :: Proxy n -> f -> HList xs -> r
     hCurry' :: Proxy n -> (HList xs -> r) -> f
@@ -40,6 +33,9 @@ instance (HCurry' n b xs r) => HCurry' (HSucc n) (x -> b) (x ': xs) r where
 
 hUncurry f = hUncurry' (arityOf f) f
 
+-- | Note: with ghc-7.10 the Arity constraint added here does not work
+-- properly with hCompose, so it is possible that other uses of 'hCurry'
+-- are better served by @hCurry' Proxy@.
 hCurry f = let f' = hCurry' (arityOf f') f
            in f'
 
@@ -69,7 +65,7 @@ has id with different types than @(.) i1 i2 i3 x == (i1 (i2 i3)) x
 Prompted by <http://stackoverflow.com/questions/28932054/can-hlistelim-be-composed-with-another-function>
 
 -}
-hCompose f g = hCurry $ \xs -> case hSplitAt Proxy xs of
+hCompose f g = hCurry' Proxy $ \xs -> case hSplitAt Proxy xs of
         (xg,xf) -> hUncurry f (hUncurry g xg `HCons` xf)
 
 
